f6e935db3febb73aef3a225921c9dd0bcc7a5ab0
Henry Weller <http://cfd.direct>
2023-08-29 16:00:42 +0100

    includeFvModel: now searches the constant directory for local configuration files

--- src/OpenFOAM/db/functionObjects/functionObjectList/functionObjectList.C
+++ src/OpenFOAM/db/functionObjects/functionObjectList/functionObjectList.C
@@ -148,6 +148,7 @@ Foam::autoPtr<Foam::functionObjectList> Foam::functionObjectList::New
                 args["func"],
                 functionsDict,
                 functionEntries::includeFuncEntry::functionObjectDictPath,
+                "system",
                 {"command", args.commandLine()},
                 region
             );
@@ -165,6 +166,7 @@ Foam::autoPtr<Foam::functionObjectList> Foam::functionObjectList::New
                     funcs[i],
                     functionsDict,
                     functionEntries::includeFuncEntry::functionObjectDictPath,
+                    "system",
                     {"command", args.commandLine()},
                     region
                 );
